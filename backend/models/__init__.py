721b7958b4d8d1a12e3b31b436d8ba5a094d12cb
Dennisco12 <akinwonjowodennisco@gmail.com>
2022-12-10 14:00:21 -0800

    Completed the filestorage set up

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 
+from os import getenv
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
 
-storage = FileStorage()
+if getenv('COBALLO_STORAGE_TYPE') == 'db':
+    storage = DBStorage()
+else:
+    storage = FileStorage()
+
 storage.reload()
