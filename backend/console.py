49a82017bfdce8176a27cb539bb4ace7be36a71f
Dennisco12 <akinwonjowodennisco@gmail.com>
2022-12-10 14:06:20 -0800

    Started setting up the console

--- backend/console.py
+++ backend/console.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""This is a console to manage the operations on our objects"""
+
+import cmd
+from models.user import User
+from models.project import Project
+
+
+class Coballo(cmd.Cmd):
+    """Class definition"""
+
+    promt = '(coballo) '
+
+    def do_quit(self):
+        """Exits the console"""
+        return True
