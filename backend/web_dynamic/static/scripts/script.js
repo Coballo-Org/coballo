564f20550729d16b5f9c99c8af9d2f3aa1bc709e
Dennisco <akinwonjowodennisco@gmail.com>
2022-12-17 15:10:32 +0000

    Corrected the script file

--- backend/web_dynamic/static/scripts/script.js
+++ backend/web_dynamic/static/scripts/script.js
@@ -4,23 +4,23 @@ $('document').ready(function () {
 	  const api = 'http://' + window.location.hostname;
 	// Create new user
 	button = document.getElementById("signup_button")
-	button.onclick( function() {
+	button.on('click', function() {
 		const req = new XMLHttpRequest();
 		const newUser = {
-			name: $("INPUT#fname") + $("INPUT#lname")
-			email: $("INPUT#email")
-			password: $("INPUT#pword")
+			name: $("INPUT#fname").val() + $("INPUT#lname").val(),
+			email: $("INPUT#email").val(),
+			password: $("INPUT#pword").val(),
 		};
-
-		req.open('POST', 'http://0.0.0.0:5000/coballo/users');
-		req.addEventListener('Load', function() {
-			if (req.status === 201 && req.readyState === 4) {
-				const res = JSON.parse(req.responseText);
-				alert(res.name + "account has been created succesfully");
-			} else {
-				alert("An error has ocurred and the account cannot be created");
+		$.ajax({
+			type: 'POST',
+			url: '/coballo/users',
+			data: newUser,
+			success: function(User) {
+				alert('User ' + User.name + " created");
+			}
+			error: function() {
+				alert("An error has occured");
 			}
 		});
-		req.send(JSON.stringify(newUser));
 	});
 });
