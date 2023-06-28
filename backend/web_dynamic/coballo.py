e7297427a941b341ab4fc06182b7e1e6fa2af072
Dennisco12 <akinwonjowodennisco@gmail.com>
2023-06-28 06:24:20 +0100

    Completed changes

--- backend/web_dynamic/coballo.py
+++ backend/web_dynamic/coballo.py
@@ -2,28 +2,14 @@
 """The entry point for the app"""
 
 from flask import Flask, render_template
-from models.user import User
-from models.project import Project
-from models import storage
 
 app = Flask(__name__)
-
-
-@app.teardown_appcontext
-def teardown_db(exception):
-    """This is called after each request to remove the current
-    sqlalchemy session"""
-    storage.close()
 
 
 @app.route('/', strict_slashes=False)
 def coballo():
     """This sets up the index template"""
-    users = storage.all(User).values()
-    projects = storage.all(Project).values()
-    return render_template('index.html',
-                            users=users,
-                            projects=projects)
+    return render_template('index.html')
 
 
 
